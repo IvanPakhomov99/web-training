7c5d7261c5c48be057cab801993cfcbd0dbfdc4a
ІванПахомов <@DESKTOP-EQV2UAF>
2018-11-30 19:27:31 +0300

    I was create User and UserTest classes Added Junit in the pom.xml

--- src/main/java/pakhomov/labs/User.java
+++ src/main/java/pakhomov/labs/User.java
@@ -1,13 +1,77 @@
 package pakhomov.labs;
 
+import java.io.Serializable;
+import java.util.Date;
+import java.time.ZoneId;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 /**
  * Hello world!
  *
  */
-public class User
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SuppressWarnings("serial")
+public class User implements Serializable {
+	private String firstName;
+    private String lastName;
+    private Long id;
+    private Date birthday;
+    
+    public User(Long id, String firstName, String lastName, Date birthday) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
     }
+    //Gettes and Setters for Id, FirstName, LastName and Birthday
+    public Long getId() {
+        return id;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public Date getBirthday() {
+        return birthday;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+    
+    //with help StringBuilder we can working with Strings without any problems like when we have a lot of inforamations 
+    public String getFullName() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(lastName)
+            .append(", ")
+            .append(firstName);
+        return builder.toString();
+    }
+    
+    //LocalDate 
+    public int getAge() {
+    	ZoneId zoneId = ZoneId.systemDefault();
+    	Instant inst = birthday.toInstant();
+        LocalDate currentLocalDate = LocalDate.now(zoneId);
+        LocalDate birthLocalDate = inst.atZone(zoneId).toLocalDate();
+        return (int) ChronoUnit.YEARS.between(birthLocalDate, currentLocalDate);
+    }
+
 }
--- src/test/java/pakhomov/labs/UserTest.java
+++ src/test/java/pakhomov/labs/UserTest.java
@@ -1,38 +1,100 @@
 package pakhomov.labs;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class UserTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public UserTest( String testName )
-    {
-        super( testName );
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Before;
+
+import org.junit.Test;
+
+public class UserTest {
+	private User user;
+    private Calendar calendar;
+    private Date birthday;
+    
+    @Before
+    public void setUp() throws ParseException {
+    	Date date = new SimpleDateFormat("dd-MM-yyyy").parse("07-07-199");
+        user = new User(1L, "Ivan", "Ivanov", date);
+        calendar = Calendar.getInstance();
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( UserTest.class );
+    
+    @Test
+    public void testGetFullName() {
+    	assertEquals("Ivanov, Ivan", user.getFullName());
     }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    
+    //today
+    @Test
+    public void test_getAge_1() {
+        calendar.set(1999, calendar.get(Calendar.NOVEMBER), 30);
+        birthday = calendar.getTime();
+        user.setBirthday(birthday);
+        int result = 19;	
+        int actual = user.getAge();
+        assertEquals(result, actual);
+    }
+    
+    //tomorrow
+    @Test
+    public void test_getAge_2() {
+    	calendar.set(1999, calendar.get(Calendar.DECEMBER), 1);
+		birthday = calendar.getTime();
+	    user.setBirthday(birthday);
+		int result = 18;	
+	    int actual = user.getAge();
+	    assertEquals(result, actual);
+    }
+  //yesterday
+    @Test
+    public void test_getAge_3() {
+    	calendar.set(1999, calendar.get(Calendar.NOVEMBER), 29);
+        birthday = calendar.getTime();
+        user.setBirthday(birthday);
+        int result = 19;	
+        int actual = user.getAge();
+        assertEquals(result, actual);
+    }
+    //before current month
+    @Test
+    public void test_getAge_4() {
+    	calendar.set(1999, calendar.get(Calendar.AUGUST), 30);
+        birthday = calendar.getTime();
+        user.setBirthday(birthday);
+        int result = 19;	
+        int actual = user.getAge();
+        assertEquals(result, actual);
+    }
+    //after current month
+    @Test
+    public void test_getAge_5() {
+    	calendar.set(1999, calendar.get(Calendar.DECEMBER), 30);
+        birthday = calendar.getTime();
+        user.setBirthday(birthday);
+        int result = 18;	
+        int actual = user.getAge();
+        assertEquals(result, actual);
+    }
+    @Test
+    public void test_getAge_6() {
+    	calendar.set(1998, calendar.get(Calendar.DECEMBER), 30);
+        birthday = calendar.getTime();
+        user.setBirthday(birthday);
+        int result = 19;	
+        int actual = user.getAge();
+        assertEquals(result, actual);
+    }
+    @Test
+    public void test_getAge_7() {
+    	calendar.set(2001, calendar.get(Calendar.DECEMBER), 30);
+        birthday = calendar.getTime();
+        user.setBirthday(birthday);
+        int result = 16;	
+        int actual = user.getAge();
+        assertEquals(result, actual);
     }
 }
