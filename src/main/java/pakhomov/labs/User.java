91c2603fb8dc5e5c4dd56f0dd75ee26b074562d1
ІванПахомов <@DESKTOP-EQV2UAF>
2018-11-30 17:43:48 +0300

    There was create project and some settings for work

---
+++ src/main/java/pakhomov/labs/User.java
@@ -0,0 +1,13 @@
+package pakhomov.labs;
+
+/**
+ * Hello world!
+ *
+ */
+public class User
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
