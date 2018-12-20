123a66ecdbe4e024535386bf420ab4c8aa0e5ccc
ІванПахомов <@DESKTOP-EQV2UAF>
2018-12-21 00:37:33 +0300

    Implementation BrowseServlet class and jsp file for display list of users

--- src/main/java/pakhomov/labs/web/BrowseServlet.java
+++ src/main/java/pakhomov/labs/web/BrowseServlet.java
@@ -1,5 +1,32 @@
 package pakhomov.labs.web;
 
-public class BrowseServlet {
+import pakhomov.labs.User;
+import pakhomov.labs.db.DaoFactory;
+import pakhomov.labs.db.DatabaseException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+
+public class BrowseServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            browse(req, resp);
+    }
+	
+	private void browse(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Collection<User> users = DaoFactory.getInstance().getUserDao().findAll();
+            req.getSession(true).setAttribute("users", users);
+            req.getRequestDispatcher("/browse.jsp").forward(req, resp);
+        } catch (DatabaseException e) {
+            throw new ServletException(e);
+        }
+    }
 }
