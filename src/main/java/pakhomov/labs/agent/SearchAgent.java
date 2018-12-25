0550f97e67c4625a6ee8cd125b55aec986581e4e
ІванПахомов <@DESKTOP-EQV2UAF>
2018-12-25 17:41:39 +0300

    added exception class SearchException, added two methods in the class SearchAgent(search(Agent's able to search users records, showUsers) but this methods aren't complete.

--- src/main/java/pakhomov/labs/agent/SearchAgent.java
+++ src/main/java/pakhomov/labs/agent/SearchAgent.java
@@ -1,8 +1,22 @@
 package pakhomov.labs.agent;
 
+import java.util.Collection;
+import pakhomov.labs.User;
+import pakhomov.labs.agent.Exceptions.SearchException;
+import pakhomov.labs.db.DaoFactory;
+import pakhomov.labs.db.DatabaseException;
 import jade.core.Agent;
 
+/**
+ * @author ІванПахомов
+ *
+ */
 public class SearchAgent extends Agent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void setup() {
@@ -16,4 +30,20 @@ public class SearchAgent extends Agent {
 		System.out.println(getAID().getName() + " terminated");
 	}
 
+	public void search(String firstName, String lastName) throws SearchException {
+		try {
+            Collection<User> users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
+            if (users.size() > 0) {
+                showUsers(users);
+            } else {
+                //
+            }
+        } catch (DatabaseException e) {
+            throw new SearchException(e);
+        }
+	}
+
+	public void showUsers(Collection<User> users) {
+		//
+	}
 }
